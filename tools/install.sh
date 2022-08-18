25fed08b8c0eae06c9ede41f33a8986b3545c0da
Nick Girga <contact@nickgirga.com>
2022-08-17 23:30:00 -0700

    create installer desktop file

--- tools/install.sh
+++ tools/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Inform user of installation
+zenity --info --text="Installing Backboard..."
+
 # Clone repository
 echo Cloning Backboard repository...
+zenity --info --text="Cloning Backboard repository..."
 git clone https://gitlab.com/nickgirga/steam-deck-backboard.git "$HOME/.local/share/backboard"
 
 # Check the exit code of git
@@ -9,10 +13,12 @@ git_exit_code="$?"
 if ! [[ "$git_exit_code" == "0" ]];
 then
 	echo -e "ERROR!: Something happened while cloning the repository (exit code $git_exit_code)!\n"
+	zenity --info --text="ERROR!: Something happened while cloning the repository (exit code $git_exit_code)!"
 	exit 1
 fi
 
 echo -e "Finished cloning repository!\n"
+zenity --info --text="Finished cloning repository!"
 
 # Symlink desktop file to applications directory
 ln -s "$HOME/.local/share/backboard/tools/backboard.desktop" "$HOME/.local/share/applications/backboard.desktop"
@@ -22,7 +28,10 @@ ln_exit_code="$?"
 if ! [[ "$ln_exit_code" == "0" ]];
 then
 	echo -e "ERROR!: Something happened while creating a symbolic link for the applications directory (exit code $ln_exit_code)!\n"
+	zenity --info --text="ERROR!: Something happened while creating a symbolic link for the applications directory (exit code $ln_exit_code)!"
 	exit 2
 fi
 
 echo -e "Created symbolic link for applications directory!\n\nDone!\n"
+zenity --info --text="Created symbolic link for applications directory!"
+zenity --info --text="Finished installing Backboard!"
