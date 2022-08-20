6fb194e96ce034c0339591196870befee7057ff9
Nick Girga <contact@nickgirga.com>
2022-08-20 09:56:28 -0700

    check for dependencies during install
    installers will no longer continue if base64 is not accessible, Decky is
    not installed, or CSS Loader is not installed

--- tools/install.sh
+++ tools/install.sh
@@ -8,6 +8,30 @@ then
 	exit
 fi
 
+# Check for base64
+if [ "`command -v base64`" == "" ];
+then
+    echo -e "Fatal Error: The \"base64\" command is not accessible! Cannot continue.\n"
+	zenity --error --ellipsize --text="Fatal Error: The \"base64\" command is not accessible! Cannot continue."
+	exit 1
+fi
+
+# Check if Decky is installed
+if [ ! -d "$HOME/homebrew" ];
+then
+    echo -e "Fatal Error: Decky does not appear to be installed! Please install it before continuing.\n"
+	zenity --error --ellipsize --text="Decky does not appear to be installed! Please <a href=\"https://github.com/SteamDeckHomebrew/decky-loader#installation\">install it</a> before continuing."
+    exit 2
+fi
+
+# Check if CSS Loader is installed
+if [ ! -d "$HOME/homebrew/plugins/SDH-CssLoader" ];
+then
+    echo -e "Fatal Error: CSS Loader does not appear to be installed! Please install it before continuing.\n"
+	zenity --error --ellipsize --text="CSS Loader does not appear to be installed! Please <a href=\"https://github.com/suchmememanyskill/SDH-CssLoader#installation\">install it</a> before continuing."
+	exit 3
+fi
+
 # Ask user if they would like to install
 zenity --question --text="Are you sure you would like to install Backboard?"
 if ! [[ "$?" == "0" ]];
@@ -30,7 +54,7 @@ if ! [[ "$git_exit_code" == "0" ]];
 then
 	echo -e "ERROR!: Something happened while cloning the repository (exit code $git_exit_code)!\n"
 	zenity --error --text="ERROR!: Something happened while cloning the repository (exit code $git_exit_code)!"
-	exit 1
+	exit 4
 fi
 
 echo -e "Finished cloning repository!\n"
@@ -45,7 +69,7 @@ if ! [[ "$ln_exit_code" == "0" ]];
 then
 	echo -e "ERROR!: Something happened while creating a symbolic link for the applications directory (exit code $ln_exit_code)!\n"
 	zenity --error --text="ERROR!: Something happened while creating a symbolic link for the applications directory (exit code $ln_exit_code)!"
-	exit 2
+	exit 5
 fi
 
 echo -e "Created symbolic link for applications directory!\n\nDone!\n"
--- tools/releases/v1.0/install.sh
+++ tools/releases/v1.0/install.sh
@@ -11,6 +11,30 @@ then
 	exit
 fi
 
+# Check for base64
+if [ "`command -v base64`" == "" ];
+then
+    echo -e "Fatal Error: The \"base64\" command is not accessible! Cannot continue.\n"
+	zenity --error --ellipsize --text="Fatal Error: The \"base64\" command is not accessible! Cannot continue."
+	exit 1
+fi
+
+# Check if Decky is installed
+if [ ! -d "$HOME/homebrew" ];
+then
+    echo -e "Fatal Error: Decky does not appear to be installed! Please install it before continuing.\n"
+	zenity --error --ellipsize --text="Decky does not appear to be installed! Please <a href=\"https://github.com/SteamDeckHomebrew/decky-loader#installation\">install it</a> before continuing."
+    exit 2
+fi
+
+# Check if CSS Loader is installed
+if [ ! -d "$HOME/homebrew/plugins/SDH-CssLoader" ];
+then
+    echo -e "Fatal Error: CSS Loader does not appear to be installed! Please install it before continuing.\n"
+	zenity --error --ellipsize --text="CSS Loader does not appear to be installed! Please <a href=\"https://github.com/suchmememanyskill/SDH-CssLoader#installation\">install it</a> before continuing."
+	exit 3
+fi
+
 # Ask user if they would like to install
 zenity --question --text="Are you sure you would like to install Backboard $version?"
 if ! [[ "$?" == "0" ]];
@@ -33,7 +57,7 @@ if ! [[ "$git_exit_code" == "0" ]];
 then
 	echo -e "ERROR!: Something happened while cloning the $version repository (exit code $git_exit_code)!\n"
 	zenity --error --text="ERROR!: Something happened while cloning the $version repository (exit code $git_exit_code)!"
-	exit 1
+	exit 4
 fi
 
 echo -e "Finished cloning $version repository!\n"
@@ -48,7 +72,7 @@ if ! [[ "$ln_exit_code" == "0" ]];
 then
 	echo -e "ERROR!: Something happened while creating a symbolic link for the applications directory (exit code $ln_exit_code)!\n"
 	zenity --error --text="ERROR!: Something happened while creating a symbolic link for the applications directory (exit code $ln_exit_code)!"
-	exit 2
+	exit 5
 fi
 
 echo -e "Created symbolic link for applications directory!\n\nDone!\n"
